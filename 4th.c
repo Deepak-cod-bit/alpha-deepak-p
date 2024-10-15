7c25a78f3be90e9819c5cd63234ad4ff5f7d418d
Deepak-cod-bit <deepakkumarparida74@gmail.com>
2024-10-15 23:12:37 +0530

    Add files via upload

---
+++ 4th.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int x,y;
+    printf("enter your number:");
+    scanf("%d",&x);
+    printf("enter your number2:");
+    scanf("%d",&y);
+    printf("the sum is %d and%d id %d",x,y,x+y);
+    return 0;
+    
+
+}
